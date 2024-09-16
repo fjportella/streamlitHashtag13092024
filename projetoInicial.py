935315a3bcabf6b2503cfb3d6fb85ad0e2ee4b6f
Fernando Portella <59586318+fjportella@users.noreply.github.com>
2024-09-16 00:07:22 +0000

    todos ativos

--- projetoInicial.py
+++ projetoInicial.py
@@ -18,19 +18,9 @@ def carregar_dados(empresas):
     cotacoes_acao = cotacoes_acao["Close"] #com 2 colchetes ele retornar uma lista de valores com o índice que é a coluna Date
     return cotacoes_acao
 
-@st.cache_data #usa o cache para não ficar carregando os dados o tempo todo
-def carregar_tickers_acoes():
-    base_tickers = pd.read_csv("IBOV.csv", sep=";") #neste caso como o separador é ; foi necessário passar o sep
-    tickers = list(base_tickers["Código"]) #transformando a planilha em uma lista python para montar os gráficos
-    tickers = [item + ".SA" for item in tickers] #concatenando .SA no final de cada item da lista (list comprehension)
-    return tickers
-
-acoes = carregar_tickers_acoes()
+acoes = ["ITUB4.SA", "PETR4.SA", "MGLU3.SA", "VALE3.SA", "ABEV3.SA", "GGBR4.SA"]
 dados = carregar_dados(acoes)
 #print(dados)
-
-# Exibir as colunas disponíveis
-#st.write("Colunas disponíveis:", dados.columns.tolist())
 
 # criar a interface do streamlit
 # para rodar o programa tem que digitar no termina: streamlit run main.py
@@ -67,54 +57,4 @@ dados = dados.loc[intervalo_data[0] : intervalo_data[1]]  #filtra as linhas de a
 
 # criar o gráfico
 st.line_chart(dados)
-
-# criar o título para performance dos ativos
-
-# calculo de perfomance
-texto_performance_ativos = ""
-
-if len(lista_acoes)==0:
-    lista_acoes = list(dados.columns)
-elif len(lista_acoes)==1:
-    dados = dados.rename(columns={"Close": acao_unica}) #quando não encontra a ação na lista, renomeia Close para acao_unica
-
-
-carteira = [1000 for acao in lista_acoes] #para cada ação na minha lista de ações eu tenho R$1.000,00
-total_inicial_carteira = sum(carteira)
-
-for i, acao in enumerate(lista_acoes): #enumerate pega também o índice para mostrar a posição da acao na lista_acoes
-    performance_ativo = dados[acao].iloc[-1] / dados[acao].iloc[0] - 1
-    performance_ativo = float(performance_ativo)
-
-    carteira[i] = carteira[i] * (1 + performance_ativo)
-
-    if performance_ativo > 0:
-        # :cor[texto]
-        texto_performance_ativos = texto_performance_ativos + f"  \n{acao}: :green[{performance_ativo:.1%}]"
-    elif performance_ativo < 0:
-        texto_performance_ativos = texto_performance_ativos + f"  \n{acao}: :red[{performance_ativo:.1%}]"
-    else:
-        texto_performance_ativos = texto_performance_ativos + f"  \n{acao}: {performance_ativo:.1%}"
-
-total_final_carteira = sum(carteira)
-performance_carteira = total_final_carteira / total_inicial_carteira - 1
-
-if performance_carteira > 0:
-    texto_performance_carteira = f"Performance da carteira com todos os ativos: :green[{performance_carteira:.1%}]"
-elif performance_carteira < 0:
-    texto_performance_carteira = f"Performance da carteira com todos os ativos: :red[{performance_carteira:.1%}]"
-else:
-    texto_performance_carteira = f"Performance da carteira com todos os ativos: {performance_carteira:.1%}"
-
-
-
-st.write(f"""
-### Performance dos Ativos
-Essa foi a perfomance de cada ativo no período selecionado:
-
-{texto_performance_ativos}
-
-{texto_performance_carteira}
-""")
-
 
